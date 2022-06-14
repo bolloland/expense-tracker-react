dbe09b449d96bf421b9d3939f2a43171fd78a9d7
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-14 18:21:03 -0500

    updated components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 
 function App() {
   return (
     <div>
-      Hello World!!
+      <Header />
+        <div classname="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
     </div>
   );
 }
