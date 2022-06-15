d20a6186423a4bed67c6d177ec0bace5633262f1
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-15 14:23:23 -0500

    useState for text and amount

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -10,14 +10,16 @@ const AddTransaction = () => {
     <h3>Add new transaction</h3>
         <form id="form">
             <div className="form-control">
-                <label htmlFor="text">Text</label>
-                <input type="text"  placeholder="Enter text..." />
+                <label htmlFor="text">Transaction:</label>
+                <input type="text" value={text} onChange={(e) => setText(e.target.value) }
+                placeholder="Enter text..." />
             </div>
             <div className="form-control">
                 <label htmlFor="amount"
-                >Amount <br />
+                >Amount: <br />
                 (negative - expense, positive - income)</label>
-                <input type="number" placeholder="Enter amount..." />
+                <input type="number" value={amount} onChange={(e) => setAmount(e.target.value) }
+                placeholder="Enter amount..." />
             </div>
         <button className="btn">Add transaction</button>
         </form>
