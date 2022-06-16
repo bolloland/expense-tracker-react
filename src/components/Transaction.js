d992553c921fa7dad7db5f1f28a7a3f211a0c088
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-16 09:54:19 -0500

    Math.abs renders pos value

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -8,6 +8,7 @@ const Transaction = ({transaction}) => {
          <li className={transaction.amount < 0 ? "minus" : "plus" }> 
          {/* ^^ determine classname using ternary */}
         {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
+        {/* Math.abs will take absolute value */}
         <button className="delete-btn">x</button>
         </li>
     </div>
