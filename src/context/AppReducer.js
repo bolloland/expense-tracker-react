18cd4932de6760c448de1386897d813651c4b47d
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-20 14:31:28 -0500

    write DELETE TRANSACTION reducer

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,10 +1,10 @@
 export default function appReducer (state, action) {
     switch(action.type) {
         case 'DELETE_TRANSACTION':
-            return {
-                ...state,
-                transactions:
-            }
+           return{
+            ...state,  //<<sends down current state
+            transactions: state.transactions.filter(trans => trans.id !== action) //filter through that state and take everything except for what we're deleting
+           }
         default:
         return state;
     }
