193c1ce1fadf39d71d682cfb039c4c7551337413
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-20 15:23:17 -0500

    add deleteTransaction to 'delete button'

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 const Transaction = ({transaction}) => {
+    const { deleteTransaction } = useContext(GlobalContext)
+
     const sign = transaction.amount < 0 ? '-' : '+';
 
   return (
     <div>
-         <li className={transaction.amount < 0 ? "minus" : "plus" }> 
-         {/* ^^ determine classname using ternary */}
-        {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
-        {/* Math.abs will take absolute value */}
-        <button className="delete-btn">x</button>
+        <li className={transaction.amount < 0 ? "minus" : "plus" }> 
+        {transaction.text} 
+        <span>{sign}${Math.abs(transaction.amount)}</span>
+        
+        <button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
         </li>
     </div>
   )
