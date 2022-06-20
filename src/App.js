f8955e5608cd0408a2ec1aed564d9af67230f558
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-20 16:47:34 -0500

    className edit (line 14)

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <GlobalProvider>
       <Header />
-      <div classname="container">
+      <div className="container">
         <Balance />
         <IncomeExpenses />
         <TransactionList />
