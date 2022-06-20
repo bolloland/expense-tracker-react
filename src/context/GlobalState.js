6c78cc7214f806f0084d2276c95ea008364949ee
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-20 16:21:45 -0500

    create addTransaction ACTION

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -28,9 +28,17 @@ export const GlobalProvider = ( { children } ) => {
         })
     }
 
+    const addTransaction = (transaction) => {
+        dispatch({
+            type: 'ADD_TRANSACTION', 
+            payload: transaction
+        })
+    }
+
     return (<GlobalContext.Provider value={{
         transactions: state.transactions, 
-        deleteTransaction
+        deleteTransaction,
+        addTransaction
         }
     }>
         {children}
