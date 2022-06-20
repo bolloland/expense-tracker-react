dde5e9ebad3ecf9d50378947998fce03af11ce64
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-20 15:31:39 -0500

    fixed action.PAYLOAD error

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,11 +1,11 @@
 export default function appReducer (state, action) {
     switch(action.type) {
         case 'DELETE_TRANSACTION':
-           return{
-            ...state,  //<<sends down current state
-            transactions: state.transactions.filter(trans => trans.id !== action) //filter through that state and take everything except for what we're deleting
+           return {
+            ...state,
+            transactions: state.transactions.filter(transaction => transaction.id !== action.payload) //filter through that state and take everything except for what we're deleting
            }
         default:
-        return state;
+            return state;
     }
 }
